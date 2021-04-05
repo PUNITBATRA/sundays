691f883d5d3e49a2acd6264700376dae9a715dd4
Punit Batra <batrapunit1999@gmail.com>
2021-04-05 18:49:41 +0530

    Added frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnlQlzj6lu2n0cZ30TD2Rh6fXNJE3WnZ0",
+  authDomain: "whatsapp-d1dca.firebaseapp.com",
+  projectId: "whatsapp-d1dca",
+  storageBucket: "whatsapp-d1dca.appspot.com",
+  messagingSenderId: "767591597993",
+  appId: "1:767591597993:web:59798a11d134bfd6c826b8",
+};
